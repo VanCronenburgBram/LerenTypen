90cf8bb06c3bcf1a1372c3919aa3efe2aed18456
VanCronenburgBram <s109544@ap.be>
2021-02-01 16:47:00 +0100

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -32,6 +32,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(cors(corsOptions));
 
+app.use('/', indexRouter);
+app.use('/servers', serversRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 var debug = require('debug')('server:server');
 var http = require('http');
 
@@ -115,25 +134,5 @@ function onListening() {
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
-
-
-app.use('/', indexRouter);
-app.use('/servers', serversRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
